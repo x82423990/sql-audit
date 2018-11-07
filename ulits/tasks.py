47e4c0104bc263011f31559b520f4ea80a52cb1e
xieyifan <xieyfian@9f.com>
2018-11-07 16:27:30 +0800

    test环境

--- ulits/tasks.py
+++ ulits/tasks.py
@@ -22,21 +22,20 @@ def send_mail(to_list, personnel, sqlid, note, action_type, sqlcontent, dbname):
     for s in sqlcontent[0:1024].split(';'):
         if s:
             sqlhtml = sqlhtml + '<div>' + s + ';' + '</div>'
-    contenthtml = "<span style='margin-right:20px'>{} {}</span> <a href='http://sql.aaa.com/sql/{}'>【查看详情】</a> <p>备注：{}</p> <p>目标数据库（线上环境）：{} </p>".format(
+    contenthtml = "<span style='margin-right:20px'>{} {}</span> <a href='http://sql.aaa.com/sql/{}'>【查看详情】</a>" \
+                  " <p>备注：{}</p> <p>目标数据库（线上环境）：{} </p><p>SQL语句： </p>".format(
         personnel, title, sqlid, note, dbname)
     if len(sqlcontent) > 1024:
         sqlhtml = sqlhtml + '<div>' + '略... ...（内容比较多，可查看详情）' + '</div>'
-    me = "<" + mail_user + "@" + mail_postfix + ">"  # 这里的hello可以任意设置，收到信后，将按照设置显示
+    # me = "<" + mail_user + "@" + mail_postfix + ">"  # 这里的hello可以任意设置，收到信后，将按照设置显示
+    me = "<mail_postfix>"  # 这里的hello可以任意设置，收到信后，将按照设置显示
     msg = MIMEText(contenthtml + sqlhtml, _subtype='html', _charset='utf-8')  # 创建一个实例，这里设置为html格式邮件
     msg['Subject'] = '{} {} [{}]'.format(personnel, title, note)  # 设置主题
     msg['From'] = me
     msg['To'] = ";".join(to_list)
     try:
-        # s = smtplib.SMTP(timeout=5)
         print("start connect")
-        # s.connect(mail_host, 25, )  # 连接smtp服务器
         s = smtplib.SMTP_SSL(host=mail_host, port=465, timeout=1)
-        print("end connect")
         # s.starttls()
         s.login(mail_user, mail_pass)  # 登陆服务器
         s.sendmail(me, to_list, msg.as_string())  # 发送邮件
