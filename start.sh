c3b4d8d64939ce59c0e11882055c3c1960f81d9b
xieyifan <xieyfian@9f.com>
2018-11-27 17:18:58 +0800

    test环境

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gunicorn -c audit/gunicorn_config.py  audit.wsgi
-python manage.py celery worker --loglevel=info
+python manage.py celery worker --loglevel=info &
+python manage.py runserver 0.0.0.0:8088
